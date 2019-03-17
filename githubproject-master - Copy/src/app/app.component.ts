43a232ba3a7a8d8f7b471a280f71123323c8671a
gbhmh <46986168+gbhmh@users.noreply.github.com>
2019-03-17 03:44:09 -0500

    project

---
+++ githubproject-master - Copy/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FileSelectDirective, FileUploader} from 'ng2-file-upload';
+import { FileService } from './file.service';
+import {saveAs} from 'file-saver';
+
+const uri = 'http://localhost:4200/file/upload';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [FileService]
+})
+export class AppComponent {
+  title = 'untitled';
+  uploader: FileUploader = new FileUploader({url: uri});
+
+  attachmentList: any = [];
+
+  constructor(private fileService: FileService) {
+
+    this.uploader.onCompleteItem = (item: any, response: any , status: any, headers: any) => {
+      this.attachmentList.push(JSON.parse(response));
+    };
+  }
+
+  download(index) {
+    const filename = this.attachmentList[index].uploadname;
+
+    this.fileService.downloadFile(filename)
+      .subscribe(
+        data => saveAs(data, filename),
+        error => console.error(error)
+      );
+  }
+}
---
+++ githubproject-master - Copy/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import {FileUploadModule} from 'ng2-file-upload';
+import {FireauthService} from './fireauth.service';
+// import { PopupModule} from 'ng2-opd-popup';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FileUploadModule,
+    HttpClientModule,
+    AngularFireAuthModule,
+    // PopupModule.forRoot(),
+    AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
